604f38605edb6de9b1338ec82179dbbcac827ef6
Ahmed <ahmedeihab709@yahoo.com>
2024-02-09 07:21:06 +0200

    (refactor): pushed nextjs for frontend

---
+++ packages/next/app/page.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Header from '@/components/Header';
+import ProductsTable from '@/components/Table';
+import CreateButton from '@/components/Buttons/CreateButton';
+
+export default function ButtonUsage() {
+  return (
+    <>
+      <Header />
+      <div className='container mx-auto'>
+        <CreateButton />
+        <ProductsTable />
+      </div>
+      
+    </>
+  );
+}
